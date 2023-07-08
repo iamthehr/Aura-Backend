a9058ed772323c221269c7db7bb64db3e1f3b7bd
iamthehr <harshraj5565@gmail.com>
2023-07-08 15:14:36 +0530

    a

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://main--calm-tanuki-515f8a.netlify.app/",
   },
 });
 
